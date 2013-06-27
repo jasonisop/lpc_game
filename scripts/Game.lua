2f7863347fcd59e55085118f577e01c1a5061c85
Jason Morse <jasonisop@gmail.com>
2013-06-27 14:07:46 -0400

    Working on game class

--- scripts/Game.lua
+++ scripts/Game.lua
@@ -6,7 +6,6 @@
 	
 --]]
 
-
 require('scripts/Player')
 require('scripts/Gui_playerHud')
 require('scripts/Gui_chatWindow')
@@ -20,28 +19,26 @@ require('scripts/Splash')
 require('scripts/Menu')
 require('scripts/SoundManager')
 
-require('scripts/MapList') 				--this is a bunch map requires
+require('scripts/MapList') 														--this is a bunch map requires
 
-
-
-Game = {	currentMap 		= 1,							--the map id of the current map
-			mapList 		= {},						--holds all the maps
-			enemies 		= {},						--holds all enemies on the map
-			state 			= 'splash', 					--game states  (play, pause, menu, loading, battle) 
-			tileSize 		= 32,						--sets tileSize to 32 this is defualt size but can be changed based on map	
-			mapWidth 		= 0,						--gets set on map load
-			mapHeight 		= 0,							--gets set on map load
-			player_Health 	= 10,						--TEMP					
-			player_Water 	= 7,							--TEMP
-			width 			= love.graphics.getWidth(), 	--width of the screen
-			height 			= love.graphics.getHeight(),	--height of the screen
-			compainions 	= "", 						--may be used to hold the players compainion NPC's
-			idle 			= false,						--used to see if game has be idle for a while
-			checkIdle 		= false,						--used to see if game has be idle for a while
+Game = {	currentMap 		= 1,												--the map id of the current map
+			mapList 		= {},												--holds all the maps
+			enemies 		= {},												--holds all enemies on the map
+			state 			= 'splash', 										--game states  (play, pause, menu, loading, battle) 
+			tileSize 		= 32,												--sets tileSize to 32 this is defualt size but can be changed based on map	
+			mapWidth 		= 0,												--gets set on map load
+			mapHeight 		= 0,												--gets set on map load
+			player_Health 	= 10,												--TEMP					
+			player_Water 	= 7,												--TEMP
+			width 			= love.graphics.getWidth(), 						--width of the screen
+			height 			= love.graphics.getHeight(),						--height of the screen
+			compainions 	= "", 												--may be used to hold the players compainion NPC's
+			idle 			= false,											--used to see if game has be idle for a while
+			checkIdle 		= false,											--used to see if game has be idle for a while
 			
-			player			= Player:new{x=10*Game.tileSize,y=14*Game.tileSize},
-			splashScreen 	= SplashScreen:new(),
-			playerHud 		= PlayerHud:new(), 
+			player			= Player:new{x=10*Game.tileSize,y=14*Game.tileSize},--player class
+			splashScreen 	= SplashScreen:new(),								--splash screen will also show starting credits
+			playerHud 		= PlayerHud:new(), 									
 			chatWindow 		= ChatWindow:new(),
 			hotbar 			= Hotbar:new(),
 			characterScreen	= CharacterScreen:new(),
@@ -49,7 +46,7 @@ Game = {	currentMap 		= 1,							--the map id of the current map
 			inventoryscreen = InventoryScreen:new(),
 			menu 			= Menu:new(),
 			soundManager	= SoundManager:new(),
-			jupiter 		= require ('scripts/jupiter'),
+			jupiter 		= require ('scripts/jupiter'),						--Class for saving and loading might use if database fails
 		}
 --gameData = {_fileName = "gameSave.txt", Game}
 --success = jupiter.save(gameData)
@@ -80,7 +77,7 @@ function Game:registerMap(...)
 	for k,map in ipairs(arg) do table.insert(self.mapList,map) end
 end
 
-local OutsideMap = Outside:new()
-local InsideMap = Inside2:new()
+local OutsideMap	= Outside:new()
+local InsideMap 	= Inside2:new()
 
 Game:registerMap(OutsideMap,InsideMap)
