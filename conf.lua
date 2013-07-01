93b14aded740ebbc64acf0f5cc8d898454b69916
MadByte <larsich30@yahoo.de>
2013-07-01 20:10:52 +0200

    removed not needed files

--- conf.lua
+++ conf.lua
@@ -1,6 +1,15 @@
 function love.conf(t)
-	t.screen.vsync = true
 	t.title = "Legends of Rathnor"
     t.author = "Jason Morse"
 	
+	t.screen.width = 800
+	t.screen.height = 600
+	t.screen.vsync = true
+	t.screen.fsaa = 0
+	t.modules.audio = true
+	t.modules.event = true
+	t.modules.sound = true
+	t.modules.joystick = false
+	t.modules.physics = false
+	t.console = false
 end
