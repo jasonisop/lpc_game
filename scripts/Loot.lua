7ae68966bb743c30991db0bb9363e1b41b440d7a
Jason Morse <jasonisop@gmail.com>
2013-07-01 16:16:06 -0400

    Working on Loot class

--- scripts/Loot.lua
+++ scripts/Loot.lua
@@ -7,10 +7,13 @@
 --]]
 
 Loot = { 
-		exp 		= 0,
-		items 		= {},
-		chest		= false,
-		chestType 	= "",
+		exp 		= 0, 			--amount of exp to add to the player
+		items 		= {},			--holds any items in the chest
+		chest	 	= false,		--if there is a phyical object to spawn
+		chestType 	= "",			--what type of object to spawn
+		looted		= false,		--if this has be looted allready
+		respawn		= false,		--if you can respawn
+		respawnTime = 120,  		--this is time in seconds
 
 		}
 
@@ -25,6 +28,7 @@ end
 function Loot:update(dt)
 end
 
+--will draw a loot chest or what ever object the loot is if there is one. also will need to draw the loot gui window, or add a loot window to inventory
 function Loot:draw()
 end	
 
@@ -32,5 +36,14 @@ function Loot:mousereleased(x, y, button)
 end
 
 function Loot:mousepressed(x, y, button)
+end
+
+function Loot:award()
+	
+	--check if this has been looted before
+	if self.looted == false then
+
+	end
+
 end
 
